c2fbed2608369a109f24efe8efc1da8fcfe7af85
Vikas Kumar <45943546+pande17827@users.noreply.github.com>
2025-01-08 06:46:55 -0800

    Updated all files

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,13 @@
+import Footer from "./components/Footer/Footer"
+import Navbar from "./components/Navbar/Navbar"
 
 
 function App() {
   return (
     <>
       <h1>Pandey</h1>
-      <N
+      <Navbar></Navbar>
+      <Footer></Footer>
     </>
   )
 }
