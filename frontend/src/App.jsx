8235232dca264fdebbaa0fbf13c92f38034a7b3a
Vikas Kumar <45943546+pande17827@users.noreply.github.com>
2025-01-08 07:18:39 -0800

    Updated all files

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,7 +6,9 @@ import Navbar from "./components/Navbar/Navbar"
 function App() {
   return (
     <>
-      
+      <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
       <Navbar></Navbar>
       <Home></Home>
       <Footer></Footer>
